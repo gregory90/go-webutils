d057a2d6f6618f7d6d9355ddee96a60ad0b74820
Grzegorz Graczyk <gregory90@gmail.com>
2015-09-29 10:55:36 +0200

    add transact function

---
+++ db/transact.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+)
+
+func Transact(db *sql.DB, txFunc func(*sql.Tx) error) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			switch p := p.(type) {
+			case error:
+				err = p
+			default:
+				err = fmt.Errorf("%s", p)
+			}
+		}
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+	return txFunc(tx)
+}
