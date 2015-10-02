9bbade1def0847fd882e024db8e7ec916c178b80
Grzegorz Graczyk <gregory90@gmail.com>
2015-10-02 12:47:12 +0200

    fix

--- web/errors.go
+++ web/errors.go
@@ -2,52 +2,52 @@ package web
 
 // validation error
 type ValidationError struct {
-	Error  string      `json:"error,omitempty"`
-	Fields interface{} `json:"fields,omitempty"`
+	ErrorType string      `json:"error,omitempty"`
+	Fields    interface{} `json:"fields,omitempty"`
 }
 
 func (e *ValidationError) Error() string {
-	return e.Error
+	return e.ErrorType
 }
 
 // serialization error
 type SerializationError struct {
-	Error string `json:"error"`
+	ErrorType string `json:"error"`
 }
 
 func (e *SerializationError) Error() string {
-	return e.Error
+	return e.ErrorType
 }
 
 // errors
 type NotFound struct {
-	Error string `json:"-"`
+	ErrorType string `json:"-"`
 }
 
 func (e *NotFound) Error() string {
-	return e.Error
+	return e.ErrorType
 }
 
 type NoContent struct {
-	Error string `json:"-"`
+	ErrorType string `json:"-"`
 }
 
 func (e *NoContent) Error() string {
-	return e.Error
+	return e.ErrorType
 }
 
 type Forbidden struct {
-	Error string `json:"error"`
+	ErrorType string `json:"error"`
 }
 
 func (e *Forbidden) Error() string {
-	return e.Error
+	return e.ErrorType
 }
 
 type BadRequest struct {
-	Error string `json:"error"`
+	ErrorType string `json:"error"`
 }
 
 func (e *BadRequest) Error() string {
-	return e.Error
+	return e.ErrorType
 }
