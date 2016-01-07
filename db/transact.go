1cf858c87ef1f228bc046e09950c7876656190b4
Grzegorz Graczyk <gregory90@gmail.com>
2016-01-07 10:42:52 +0100

    add retry on deadlock

--- db/transact.go
+++ db/transact.go
@@ -3,6 +3,10 @@ package db
 import (
 	"database/sql"
 	"fmt"
+
+	"github.com/gregory90/go-webutils/try"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func Transact(db *sql.DB, txFunc func(*sql.Tx) error) (err error) {
@@ -25,5 +29,13 @@ func Transact(db *sql.DB, txFunc func(*sql.Tx) error) (err error) {
 		}
 		err = tx.Commit()
 	}()
-	return txFunc(tx)
+	err = try.Do(func(attempt int) (bool, error) {
+		var err error
+		err = txFunc(tx)
+		if err != nil && err.(*mysql.MySQLError).Number == 1213 {
+			return attempt < 5, err
+		}
+		return false, err
+	})
+	return err
 }
